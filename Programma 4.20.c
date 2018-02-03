98058903d0c930d1461c157e15f32ba12f35f579
Alle Cross Costi <costialle2@hotmail.it>
2018-02-03 21:21:45 +0100

    Initial commit

---
+++ Programma 4.20.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  
+  int main () {
+  
+  int intVAR;
+  char charVAR;
+  
+  charVAR = 'C';
+  intVAR = (int)charVAR;
+  
+  printf("Il carattere %c in ASCII e %d", charVAR, intVAR);
+  
+  return 0;
+  
+  
+  }
